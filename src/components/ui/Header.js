49a4d626312eded084a8782a193ac498b6ce5561
Senadin <beganovicsenadin24@gmail.com>
2021-03-27 17:34:41 +0100

    added own styles for logo

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -10,6 +10,8 @@ import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
 import Menu from "@material-ui/core/Menu";
 
+import logo from '../../assets/logo.svg'
+
 function ElevationScroll(props) {
   const { children } = props;
 
@@ -23,14 +25,36 @@ function ElevationScroll(props) {
   });
 }
 
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+    marginBottom: "3em",
+  },
+
+  logo: {
+    height: "7em",
+    [theme.breakpoints.down("md")]: {
+      height: "7em"
+    },
+    [theme.breakpoints.down("xs")]: {
+      height: "5.5em"
+    }
+  },
+}));
+
 export default function Header(props) {
+  const classes = useStyles();
+
   return (
-    <ElevationScroll>
-      <AppBar position="fixed">
-        <Toolbar>
-          <Typography variant="h3"> Arc Development</Typography>
-        </Toolbar>
-      </AppBar>
-    </ElevationScroll>
+    <React.Fragment>
+      <ElevationScroll>
+        <AppBar position="fixed">
+          <Toolbar disableGutters>
+            <img src={logo} className={classes.logo} alt="company logo"/>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </React.Fragment>
   );
 }
