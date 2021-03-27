94fc2f757e600d80cf892f88fbb07dd660e60ab8
Senadin <beganovicsenadin24@gmail.com>
2021-03-27 17:19:22 +0100

    setting up material-ui and themes for it

---
+++ src/components/ui/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import { makeStyles } from "@material-ui/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
+import Menu from "@material-ui/core/Menu";
+
+function ElevationScroll(props) {
+  const { children } = props;
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+export default function Header(props) {
+  return (
+    <ElevationScroll>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h3"> Arc Development</Typography>
+        </Toolbar>
+      </AppBar>
+    </ElevationScroll>
+  );
+}
