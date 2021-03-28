630c6ea4246a3d964d1c940c152a509c5b5a0ed7
Senadin <beganovicsenadin24@gmail.com>
2021-03-28 18:54:06 +0200

    Added routes for each page

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
@@ -31,12 +31,18 @@ const useStyles = makeStyles((theme) => ({
   },
 
   logo: {
-    height: "7em",
+    height: "8em",
     [theme.breakpoints.down("md")]: {
       height: "7em"
     },
     [theme.breakpoints.down("xs")]: {
       height: "5.5em"
+    }
+  },
+  logoContainer:{
+    padding: 0,
+    "&:hover":{
+      backgroundColor: "transparent",
     }
   },
   tabContainer:{
@@ -61,19 +67,43 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Header(props) {
   const classes = useStyles();
+  const [value, setValue] = useState(0);
+
+  const handleChange = (e, value) => {
+    setValue(value);
+  }
+
+  useEffect(() => {
+    if(window.location.pathname === "/" && value !== 0){
+      setValue(0)
+    }  else if(window.location.pathname === "/services" && value !== 1) {
+      setValue(1)
+    }else if(window.location.pathname === "/revolution" && value !== 2) {
+      setValue(2)
+    }else if(window.location.pathname === "/about" && value !== 3) {
+      setValue(3)
+    }else if(window.location.pathname === "/contacts" && value !== 4) {
+      setValue(4)
+    }else if(window.location.pathname === "/estimate" && value !== 5) {
+      setValue(5)
+    }
+  }, [value])
+
 
   return (
     <React.Fragment>
       <ElevationScroll>
         <AppBar position="fixed">
           <Toolbar disableGutters>
-            <img src={logo} className={classes.logo} alt="company logo"/>
-            <Tabs className={classes.tabContainer}>
-              <Tab className={classes.tab} label="Home"/>
-              <Tab className={classes.tab} label="Services"/>
-              <Tab className={classes.tab} label="The Revolution"/>
-              <Tab className={classes.tab} label="About Us"/>
-              <Tab className={classes.tab} label="Contact Us"/>
+            <Button component={Link} to="/" className={classes.logoContainer} onClick={() => setValue(0)} disableRipple >
+              <img src={logo} className={classes.logo} alt="company logo"/> 
+            </Button>
+            <Tabs value={value} className={classes.tabContainer} onChange={handleChange} indicatorColor="primary">
+              <Tab className={classes.tab} component={Link} to="/" label="Home"/>
+              <Tab className={classes.tab} component={Link} to="/services"  label="Services"/>
+              <Tab className={classes.tab} component={Link} to="/revolution"  label="The Revolution"/>
+              <Tab className={classes.tab} component={Link} to="/about" label="About Us"/>
+              <Tab className={classes.tab} component={Link} to="/contact"  label="Contact Us"/>
             </Tabs>
             <Button variant="contained" color="secondary" className={classes.button}>
               Free Estimate
